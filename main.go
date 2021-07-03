1bfeb3a9e3c9c71d7084a694b1d767cb7d338cfe
Itsaadarsh <heyitsaadarsh@gmail.com>
2021-07-03 12:49:35 +0530

    Adding HTML templates

--- main.go
+++ main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+const portNumber = ":3000"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTmpl(w, "home.page.html")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTmpl(w, "about.page.html")
+}
+
+func renderTmpl(w http.ResponseWriter, tmpl string) {
+
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Error while parsing template : ", err)
+		return
+	}
+}
+
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello Mofos!")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written %d", n))
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(portNumber, nil)
 }
